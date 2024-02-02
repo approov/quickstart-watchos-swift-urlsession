4dcc463bc3796f2dd49ee7990a55452641b7f082
ivo liondov <ivol@ivos-Mac-mini.local>
2024-02-02 09:53:03 +0000

    Working version of app without approov sdk

--- shapes-app/ShapesApp/ShapesApp Watch App/ShapesAppApp.swift
+++ shapes-app/ShapesApp/ShapesApp Watch App/ShapesAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ShapesApp_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(mImage: "approov", mText: "")
         }
     }
 }
